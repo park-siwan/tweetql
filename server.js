9035733d6b22c23898fd080dfe176186ec1df1a4
park-siwan <siwandevelop@gmail.com>
2023-09-07 21:54:13 +0900

    feat: query Resolvers

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server';
+let tweets = [
+  {
+    id: '1',
+    text: 'first one!',
+    userId: '2',
+  },
+  {
+    id: '2',
+    text: 'second one',
+    userId: '1',
+  },
+];
 
+let users = [
+  {
+    id: '1',
+    firstName: 'nico',
+    lastName: 'las',
+  },
+  {
+    id: '2',
+    firstName: 'Elon',
+    lastName: 'Mask',
+  },
+];
 const typeDefs = gql`
   type User {
     id: ID!
@@ -22,7 +46,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
-  console.log(`Runnig on ${url}`);
+  console.log(`Running on ${url}`);
 });
